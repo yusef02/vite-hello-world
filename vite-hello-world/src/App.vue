70b0a5343ed28b77a424927ee89d3deae385c411
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-13 11:40:05 +0100

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHero from "./components/AppHero.vue";
+export default {
+  data() {
+    return {
+      title: "Vite hello world",
+    };
+  },
+  components: { AppHero },
+  mounted() {
+    console.log("vue funzionante");
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <app-hero />
+</template>
+
+<style></style>
